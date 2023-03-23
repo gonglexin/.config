05d954f3130d45383596dec074dac39c14b0b6c8
Lexin Gong <gonglexin@gmail.com>
2023-03-23 10:22:02 +0800

    Add tabnine

---
+++ nvim/lua/plugins/tabnine.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "tzachar/cmp-tabnine",
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp_tabnine" } }))
+    end,
+  },
+}
