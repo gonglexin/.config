66eea4b6d0386d74a09fcc93c42f21486219a031
Lexin Gong <gonglexin@gmail.com>
2023-03-29 10:23:15 +0800

    Refact tabnine config

--- nvim/lua/plugins/tabnine.lua
+++ nvim/lua/plugins/tabnine.lua
@@ -1,11 +1,10 @@
 return {
   {
-    "tzachar/cmp-tabnine",
-    build = "./install.sh",
-    dependencies = "hrsh7th/nvim-cmp",
-  },
-  {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "tzachar/cmp-tabnine",
+      build = "./install.sh",
+    },
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp_tabnine" } }))
