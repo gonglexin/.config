6b6235994bfeba58fb04904f259132815e0f084a
Lexin Gong <gonglexin@gmail.com>
2023-03-30 23:18:44 +0800

    Treesiter: add diff

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -22,6 +22,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
+        "diff",
         "elixir",
         "heex",
         "ruby",
@@ -36,7 +37,6 @@ return {
         emmet_ls = {
           filetypes = {
             -- custom
-            "elixir",
             "heex",
 
             -- default
