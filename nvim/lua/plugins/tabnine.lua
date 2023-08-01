82268c9dfe5858f033b2995c73c6f4bf22476fa2
Lexin Gong <gonglexin@gmail.com>
2023-08-01 11:43:15 +0800

    Use `tabnine-nvim` instead of `cmp-tabnine`

--- nvim/lua/plugins/tabnine.lua
+++ nvim/lua/plugins/tabnine.lua
@@ -1,13 +1,30 @@
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "tzachar/cmp-tabnine",
+--       build = "./install.sh",
+--     },
+--     opts = function(_, opts)
+--       local cmp = require("cmp")
+--       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp_tabnine" } }))
+--     end,
+--   },
+-- }
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "tzachar/cmp-tabnine",
-      build = "./install.sh",
-    },
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp_tabnine" } }))
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    config = function()
+      require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<C-l>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      })
     end,
   },
 }
