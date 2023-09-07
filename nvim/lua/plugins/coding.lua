97d31e573b6e4452c8ec605f87da48c38c3dd165
Lexin Gong <gonglexin@gmail.com>
2023-09-07 12:50:25 +0800

    Plugin updates
    1. Enable tailwind in heex
    2. Switch `emmet-ls` to `emmet-language-server`
    3. Add plugins: `nvim-colorizer.lua` and `codeium.vim`

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -6,7 +6,7 @@ return {
         -- html & js & css
         "html-lsp",
         "css-lsp",
-        "emmet-ls",
+        "emmet-language-server",
       })
     end,
   },
@@ -24,27 +24,40 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        emmet_ls = {
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              eelixir = "html-eex",
+              eruby = "erb",
+              heex = "phoenix-heex",
+            },
+          },
+        },
+        emmet_language_server = {
           filetypes = {
             -- custom
             "heex",
 
             -- default
-            "html",
-            "typescriptreact",
-            "javascriptreact",
             "css",
+            "eruby",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "less",
             "sass",
             "scss",
-            "less",
-            "eruby",
+            "svelte",
+            "pug",
+            "typescriptreact",
+            "vue",
           },
         },
       },
       -- setup = {
       --   elixirls = function(_, opts)
       --     require("lspconfig").elixirls.setup({
-      --       cmd = { "/Users/gonglexin/projects/lexical/_build/dev/rel/lexical/start_lexical.sh" },
+      --       cmd = { "/Users/gonglexin/projects/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
       --     })
       --     return true
       --   end,
@@ -52,9 +65,55 @@ return {
     },
   },
 
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+
+  {
+    "Exafunction/codeium.vim",
+    -- event = "BufEnter",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+  -- {
+  --   {
+  --     "jcdickinson/http.nvim",
+  --     build = "cargo build --workspace --release",
+  --   },
+  --   {
+  --     "jcdickinson/codeium.nvim",
+  --     dependencies = {
+  --       "jcdickinson/http.nvim",
+  --       "nvim-lua/plenary.nvim",
+  --       "hrsh7th/nvim-cmp",
+  --     },
+  --     config = function()
+  --       require("codeium").setup({})
+  --     end,
+  --   },
+  -- },
+
   { import = "lazyvim.plugins.extras.coding.yanky" },
 
-  { import = "lazyvim.plugins.extras.lang.elixir" },
+  -- { import = "lazyvim.plugins.extras.lang.elixir" },
   { import = "lazyvim.plugins.extras.lang.ruby" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
