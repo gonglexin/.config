358877756bbf84bdaa75f5453d677339573cfcac
Lexin Gong <gonglexin@gmail.com>
2023-10-10 09:51:28 +0800

    Add codeium

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -74,44 +74,8 @@ return {
     },
   },
 
-  {
-    "Exafunction/codeium.vim",
-    -- event = "BufEnter",
-    config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true })
-      vim.keymap.set("i", "<c-;>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true })
-    end,
-  },
-  -- {
-  --   {
-  --     "jcdickinson/http.nvim",
-  --     build = "cargo build --workspace --release",
-  --   },
-  --   {
-  --     "jcdickinson/codeium.nvim",
-  --     dependencies = {
-  --       "jcdickinson/http.nvim",
-  --       "nvim-lua/plenary.nvim",
-  --       "hrsh7th/nvim-cmp",
-  --     },
-  --     config = function()
-  --       require("codeium").setup({})
-  --     end,
-  --   },
-  -- },
-
   { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.coding.codeium" },
 
   -- { import = "lazyvim.plugins.extras.lang.elixir" },
   { import = "lazyvim.plugins.extras.lang.ruby" },
