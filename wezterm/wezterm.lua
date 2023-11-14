05d459a697c710af5aeb2fd83d78226d31fe11dc
Lexin Gong <gonglexin@gmail.com>
2023-11-14 20:26:15 +0800

    :tada: Refine wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,94 +1,191 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-wezterm.on("update-right-status", function(window, pane)
-  window:set_right_status(window:active_workspace())
-end)
-
-local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
+local config = {}
+-- Use config builder object if possible
+if wezterm.config_builder then
+  config = wezterm.config_builder()
 end
 
-local config = {
-  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-  font = wezterm.font("FiraCode Nerd Font"),
-  font_size = 16.0,
-  leader = { key = "b", mods = "CTRL" },
+-- wezterm.on("update-right-status", function(window, pane)
+--   window:set_right_status(window:active_workspace())
+-- end)
 
-  keys = {
-    { key = "a", mods = "LEADER|CTRL", action = act({ SendString = "\x01" }) },
-    { key = "-", mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-    { key = "\\", mods = "LEADER", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-    { key = '"', mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-    { key = "%", mods = "LEADER", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-    { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-    { key = "c", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
-    { key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
-    { key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
-    { key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
-    { key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
-    { key = "H", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Left", 5 } }) },
-    { key = "J", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Down", 5 } }) },
-    { key = "K", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Up", 5 } }) },
-    { key = "L", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Right", 5 } }) },
-    { key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
-    { key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
-    { key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
-    { key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
-    { key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
-    { key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
-    { key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
-    { key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
-    { key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
-    { key = "&", mods = "LEADER|SHIFT", action = act({ CloseCurrentTab = { confirm = true } }) },
-    { key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
-    { key = "s", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
-    { key = " ", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
+-- local function scheme_for_appearance(appearance)
+--   if appearance:find("Dark") then
+--     return "Catppuccin Mocha"
+--   else
+--     return "Catppuccin Latte"
+--   end
+-- end
 
-    { key = "p", mods = "CMD|SHIFT", action = act.ActivateCommandPalette },
+-- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font_with_fallback({
+  { family = "Iosevka Nerd Font", scale = 1.4, weight = "Bold" },
+  { family = "FiraCode Nerd Font Mono", scale = 1.4 },
+})
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.scrollback_lines = 10000
+config.default_workspace = "main"
 
-    -- Switch to the default workspace
-    { key = "y", mods = "LEADER", action = act.SwitchToWorkspace({ name = "default" }) },
-    -- Switch to a monitoring workspace, which will have `top` launched into it
-    {
-      key = "u",
-      mods = "LEADER",
-      action = act.SwitchToWorkspace({ name = "monitoring", spawn = { args = { "top" } } }),
-    },
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5,
+}
 
-    -- Create a new workspace with a random name and switch to it
-    { key = "w", mods = "LEADER", action = act.SwitchToWorkspace },
+config.tab_bar_at_bottom = true
 
-    -- Prompt for a name to use for a new workspace and switch to it.
-    {
-      key = "n",
-      mods = "LEADER",
-      action = act.PromptInputLine({
-        description = wezterm.format({
-          { Attribute = { Intensity = "Bold" } },
-          { Foreground = { AnsiColor = "Fuchsia" } },
-          { Text = "Enter name for new workspace" },
-        }),
-        action = wezterm.action_callback(function(window, pane, line)
-          -- line will be `nil` if they hit escape without entering anything
-          -- An empty string if they just hit enter
-          -- Or the actual line of text they wrote
-          if line then
-            window:perform_action(
-              act.SwitchToWorkspace({
-                name = line,
-              }),
-              pane
-            )
-          end
-        end),
+-- Keys
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  -- Send C-b when pressing C-b twice
+  { key = "b", mods = "LEADER|CTRL", action = act.SendKey({ key = "b", mods = "CTRL" }) },
+
+  { key = "p", mods = "CMD|SHIFT", action = act.ActivateCommandPalette },
+
+  -- Pane keybindings
+  { key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+  { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+  { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+  { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+  { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+  { key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+
+  -- Tab keybindings
+  { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+  { key = " ", mods = "LEADER", action = act.ShowTabNavigator },
+  { -- Rename tab
+    key = ",",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
       }),
-    },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
+  },
+
+  -- Workspace
+  { key = "s", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+  { key = "w", mods = "LEADER", action = act.SwitchToWorkspace },
+  {
+    key = "n",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Enter name for new workspace" },
+      }),
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:perform_action(
+            act.SwitchToWorkspace({
+              name = line,
+            }),
+            pane
+          )
+        end
+      end),
+    }),
   },
 }
+
+-- Select tab by number
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+    { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+    { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+    { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter", action = "PopKeyTable" },
+  },
+}
+
+wezterm.on("update-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = "#f7768e"
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = "#7dcfff"
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = "#bb9af7"
+  end
+
+  -- Current working directory
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
+  local cwd = pane:get_current_working_dir()
+  cwd = cwd and basename(cwd) or ""
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  cmd = cmd and basename(cmd) or ""
+
+  -- Time
+  local time = wezterm.strftime("%H:%M")
+
+  -- Left status (left of the tab line)
+  window:set_left_status(wezterm.format({
+    { Foreground = { Color = stat_color } },
+    { Text = "  " },
+    { Text = wezterm.nerdfonts.md_alien_outline .. "   " .. stat },
+    { Text = "    " },
+  }))
+
+  -- Right status
+  window:set_right_status(wezterm.format({
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = "#e0af68" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+    { Text = "  " },
+  }))
+end)
+
+--[[ Appearance setting for when I need to take pretty screenshots
+config.enable_tab_bar = false
+config.window_padding = {
+  left = "0.5cell",
+  right = "0.5cell",
+  top = "0.5cell",
+  bottom = "0cell",
+}
+--]]
 
 return config
