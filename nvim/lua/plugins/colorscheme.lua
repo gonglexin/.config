bc573e07ce99c346571888cf9b92c63976043ea0
Lexin Gong <gonglexin@gmail.com>
2024-01-11 09:17:50 +0800

    :zap: Catppuccin for neovim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,11 @@
 return {
-  { "yorickpeterse/nvim-grey" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Configure LazyVim to load theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "grey",
+      colorscheme = "catppuccin",
     },
-  },
-
-  -- Configure telescope use `grey` layout
-  {
-    "telescope.nvim",
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          -- These three settings are optional, but recommended.
-          prompt_prefix = "",
-          entry_prefix = " ",
-          selection_caret = " ",
-
-          -- This is the important part: without this, Telescope windows will look a
-          -- bit odd due to how borders are highlighted.
-          layout_strategy = "grey",
-          layout_config = {
-            -- The extension supports both "top" and "bottom" for the prompt.
-            prompt_position = "top",
-
-            -- You can adjust these settings to your liking.
-            width = 0.6,
-            height = 0.5,
-            preview_width = 0.6,
-          },
-        },
-      })
-      require("telescope").load_extension("grey")
-    end,
   },
 }
