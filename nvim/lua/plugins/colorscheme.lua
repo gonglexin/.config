d4a4b8f7f8c3f3239cf8568ba039a44633d86d15
Lexin Gong <gonglexin@gmail.com>
2024-07-11 22:42:45 +0800

    :zap: Set catppuccin colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,8 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
   {
-    "f-person/auto-dark-mode.nvim",
-    config = {
-      update_interval = 1000,
-      set_dark_mode = function()
-        vim.api.nvim_set_option("background", "dark")
-        vim.cmd("colorscheme catppuccin-frappe")
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option("background", "light")
-        vim.cmd("colorscheme catppuccin-latte")
-      end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
 }
