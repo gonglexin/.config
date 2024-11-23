38d1ac4ce3e2bc8aef157a59ab1c8340380e8782
Lexin Gong <gonglexin@gmail.com>
2024-11-23 13:14:08 +0800

    :zap: Add `yetone/avante.nvim` & Switch to `blink.cmp`

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -24,15 +24,32 @@ return {
     end,
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      window = {
-        completion = require("cmp").config.window.bordered(),
-        documentation = require("cmp").config.window.bordered(),
-      },
-    },
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = {
+  --     window = {
+  --       completion = require("cmp").config.window.bordered(),
+  --       documentation = require("cmp").config.window.bordered(),
+  --     },
+  --   },
+  -- },
+
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     windows = {
+  --       autocomplete = {
+  --         border = "rounded",
+  --       },
+  --     },
+  --     documentation = {
+  --       border = "rounded",
+  --     },
+  --     signature_help = {
+  --       border = "rounded",
+  --     },
+  --   },
+  -- },
 
   {
     "neovim/nvim-lspconfig",
@@ -59,14 +76,85 @@ return {
           },
         },
       },
-      -- setup = {
-      --   elixirls = function(_, opts)
-      --     require("lspconfig").elixirls.setup({
-      --       cmd = { "/Users/gonglexin/projects/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
-      --     })
-      --     return true
-      --   end,
-      -- },
     },
   },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        ---@type AvanteProvider
+        deepseek = {
+          endpoint = "https://api.deepseek.com/chat/completions",
+          model = "deepseek-coder",
+          api_key_name = "DEEPSEEK_API_KEY",
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint,
+              headers = {
+                ["Accept"] = "application/json",
+                ["Content-Type"] = "application/json",
+                ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+              },
+              body = {
+                model = opts.model,
+                messages = { -- you can make your own message, but this is very advanced
+                  { role = "system", content = code_opts.system_prompt },
+                  { role = "user", content = require("avante.providers.openai").get_user_message(code_opts) },
+                },
+                temperature = 0,
+                max_tokens = 4096,
+                stream = true, -- this will be set by default.
+              },
+            }
+          end,
+          parse_response_data = function(data_stream, event_state, opts)
+            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+          end,
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "echasnovski/mini.icons",
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }, -- },
 }
